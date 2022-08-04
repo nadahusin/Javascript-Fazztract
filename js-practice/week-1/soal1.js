1cace36db2a0708ca628be6e3422a1b8ae0a34ed
Nada Salsabila <99460188+nadahusin@users.noreply.github.com>
2022-08-04 15:06:23 +0700

    Membuat variabel dengan value dan tipe data

---
+++ js-practice/week-1/soal1.js
@@ -0,0 +1,38 @@
+const biodata = {
+    namaLengkap: "Nada Salsabila",
+    umur: 21,
+    hobby: ["Bernyanyi", "Membaca", "Mendengarkan Musik"],
+    isMarried: false,
+    schoolList: [{
+            nama: "SMP Negeri 1 Singkawang",
+            yearIn: 2012,
+            yearOut: 2015,
+            major: null
+        },
+        {
+            nama: "SMK Negeri 5 Pontianak",
+            yearIn: 2016,
+            yearOut: 2018,
+            major: "Hotel Accommodation"
+        },
+        {
+            nama: "Akademi Komunitas Singkawang PDD Politeknik Negeri Pontianak",
+            yearIn: 2018,
+            yearOut: 2020,
+            major: "Teknik Informatika"
+        }
+    ],
+    skill: [{
+            skillName: "Coding",
+            level: "beginner"
+        },
+        {
+            skillName: "Microsoft Office (Excell, Word, PowerPoint)",
+            level: "advanced'"
+        }
+    ],
+    interestInCoding: true
+
+}
+
+console.log(biodata)
